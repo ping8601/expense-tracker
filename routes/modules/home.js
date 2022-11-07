8ccd849219e0bc7bac93f92c664738d78ae8c2bd
ping8601 <wanping86701@gmail.com>
2022-11-07 21:29:17 +0800

    connect category with icon

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,13 @@ const moment = require('moment')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const CATEGORY = {
+  家居物業: "fa-house",
+  交通出行: "fa-van-shuttle",
+  休閒娛樂: "fa-face-grin-beam",
+  餐飲食品: "fa-utensils",
+  其他: "fa-pen"
+}
 
 // index page
 router.get('/', (req, res) => {
@@ -12,6 +19,7 @@ router.get('/', (req, res) => {
       let total = 0
       records.forEach(record => {
         total += record.amount
+        record.category = CATEGORY[record.category]
         record.date = moment(record.date).format('YYYY/MM/DD')
       })
       return res.render('index', { records, total })
