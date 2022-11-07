7558a2dc53f38abee8d30bc424b1dfde946734e9
ping8601 <wanping86701@gmail.com>
2022-11-07 22:08:36 +0800

    add category model and category seeder

---
+++ models/seeders/categorySeeder.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const db = require('../../config/mongoose')
+const Category = require('../category')
+
+const categories = [{
+  name: '家居物業', 
+  icon: 'fa-house'
+}, {
+  name: '交通出行',
+  icon: 'fa-van-shuttle'
+}, {
+  name: '休閒娛樂',
+  icon: 'fa-face-grin-beam'
+}, {
+  name: '餐飲食品',
+  icon: 'fa-utensils'
+}, {
+  name: '其他',
+  icon: 'fa-pen'
+}]
+
+db.once('open', () => {
+  return Category.create(categories)
+    .then(() => {
+      console.log('Done!')
+      process.exit()
+    })
+    .catch(error => console.error(error))
+})
