0a0a8748480577f1083f102d1652c6d558809f3d
ping8601 <wanping86701@gmail.com>
2022-11-08 13:37:04 +0800

    add category filter in home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,23 +4,31 @@ const router = express.Router()
 
 const Record = require('../../models/record')
 const Category = require('../../models/category')
-const categories = {}
 
 // index page
 router.get('/', (req, res) => {
+  const categories = []
+  const selectedCategory = req.query.selectedCategory
   Category.find()
-    .then(items => items.forEach(item => categories[item.name] = item.icon))
+    .lean()
+    .then(items => items.forEach(item => {
+      if (item.name === selectedCategory) {
+        item.selected = true
+      }
+      categories.push(item)
+    }))
     .then (() => {
-      Record.find()
+      return Record.find(selectedCategory === "" || !selectedCategory ?  {} : { category: selectedCategory })
         .lean()
+        .sort({ date: 'desc'})
         .then(records => {
           let total = 0
           records.forEach(record => {
             total += record.amount
-            record.category = categories[record.category]
+            record.category = categories.find(category => category.name === record.category).icon
             record.date = moment(record.date).format('YYYY/MM/DD')
           })
-          return res.render('index', { records, total, categories: Object.keys(categories) })
+          return res.render('index', { records, total, categories })
         })
         .catch(error => console.error(error))
     })
