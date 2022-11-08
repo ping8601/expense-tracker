eb46e1827acbb3a225a8a623d8805dc2aecec01f
ping8601 <wanping86701@gmail.com>
2022-11-08 14:26:54 +0800

    add register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // login
 router.get('/login', (req, res) => {
   res.render('login')
@@ -9,6 +11,26 @@ router.get('/login', (req, res) => {
 // register
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const {name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then( user => {
+      if (user) {
+        console.log('user already exists!')
+        return res.render('register', { name, email, password, confirmPassword })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/users/login'))
+          .catch(error => console.error(error))
+      }
+    })
+    .catch(error => console.error(error))
 })
 
 // logout
